aed8cd89c18006b112f40f5d804648740e5d4bd3
Serhii Kupriienko <skupriienko@anaconda.com>
2025-07-23 18:49:25 +0300

    Debug windows

--- recipe/build-krb5.bat
+++ recipe/build-krb5.bat
@@ -1,28 +1,5 @@
 @echo off
 
-set NO_LEASH=1
-
-:: Finds stdint.h from msinttypes.
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%
-
-:: Set the install path
-set KRB_INSTALL_DIR=%LIBRARY_PREFIX%
-
-:: Need this set or libs/Makefile fails
-set VISUALSTUDIOVERSION=%VS_MAJOR%0
-
-cd src
-
-:: Create Makefile for Windows (reuse the prep step)
-nmake -f Makefile.in prep-windows
-if errorlevel 1 exit 1
-
-:: Build only the utilities we need (not the full build)
-nmake NODEBUG=1
-if errorlevel 1 exit 1
-
-:: Install everything, then we'll let conda-build's files: section handle separation
-nmake install NODEBUG=1
-if errorlevel 1 exit 1
-
-echo krb5 package build complete 
+:: For Windows, we rely on conda-build's files: section to separate packages
+:: since the source build is complex and the libkrb5 package already builds everything
+echo Windows krb5 package - using files separation from libkrb5 build 
