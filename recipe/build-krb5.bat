61110a650202518cda7806bef1f97764341a4408
Serhii Kupriienko <skupriienko@anaconda.com>
2025-07-23 15:04:04 +0300

    Debug Windows

--- recipe/build-krb5.bat
+++ recipe/build-krb5.bat
@@ -1,16 +1,7 @@
 @echo off
 
-REM Copy utility executables from staging area to final location
-REM libkrb5 moved them to staging, now krb5 copies them back to proper location
+REM krb5 package gets utilities via files: section patterns
+REM On Windows, we'll rely on the files: section to handle separation
+REM instead of staging since conda-build environments may be isolated
 
-if not exist "%LIBRARY_PREFIX%\krb5_staging\bin" (
-    echo ERROR: krb5 staging directory not found! libkrb5 must be built first.
-    exit /b 1
-)
-
-REM Copy all utility executables from staging back to bin
-xcopy "%LIBRARY_PREFIX%\krb5_staging\bin\*" "%LIBRARY_PREFIX%\bin\" /Y
-if errorlevel 1 exit 1
-
-REM Clean up staging directory
-rmdir /s /q "%LIBRARY_PREFIX%\krb5_staging" 
+echo "krb5 package build - files will be separated by conda-build files: patterns" 
