e72800ebcfca7e0d47e744c0354fc86a5c932ef0
Serhii Kupriienko <skupriienko@anaconda.com>
2025-07-23 14:09:15 +0300

    Copy files on Windows

--- recipe/build-krb5.bat
+++ recipe/build-krb5.bat
@@ -1,4 +1,16 @@
 @echo off
 
-REM krb5 package uses files: section - all files are already built by libkrb5
-REM conda-build will automatically separate files based on the files: sections 
+REM Copy utility executables from staging area to final location
+REM libkrb5 moved them to staging, now krb5 copies them back to proper location
+
+if not exist "%LIBRARY_PREFIX%\krb5_staging\bin" (
+    echo ERROR: krb5 staging directory not found! libkrb5 must be built first.
+    exit /b 1
+)
+
+REM Copy all utility executables from staging back to bin
+xcopy "%LIBRARY_PREFIX%\krb5_staging\bin\*" "%LIBRARY_PREFIX%\bin\" /Y
+if errorlevel 1 exit 1
+
+REM Clean up staging directory
+rmdir /s /q "%LIBRARY_PREFIX%\krb5_staging" 
--- recipe/build-libkrb5.bat
+++ recipe/build-libkrb5.bat
@@ -21,4 +21,27 @@ if errorlevel 1 exit 1
 
 :: Install everything first (libkrb5 builds everything)
 nmake install NODEBUG=1
-if errorlevel 1 exit 1 
+if errorlevel 1 exit 1
+
+:: Create staging directory for krb5 utilities
+mkdir "%LIBRARY_PREFIX%\krb5_staging"
+mkdir "%LIBRARY_PREFIX%\krb5_staging\bin"
+
+:: Move utility executables to staging area (will be picked up by krb5 package)
+move "%LIBRARY_PREFIX%\bin\kinit.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\klist.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\kdestroy.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\kpasswd.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\kswitch.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\kvno.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\gss-client.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\gss-server.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\kcpytkt.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\kdeltkt.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\kfwcpcc.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\mit2ms.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\ms2mit.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+move "%LIBRARY_PREFIX%\bin\ccapiserver.exe" "%LIBRARY_PREFIX%\krb5_staging\bin\"
+
+:: libkrb5 package keeps: DLLs, .lib files, headers, plugins
+:: Utility executables are now in staging area for krb5 package 
